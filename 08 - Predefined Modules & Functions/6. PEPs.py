7d9a842c3cb231839feb1ef315ca4255eee61df2
TheStxfan <stefan.ferro5@gmail.com>
2024-09-05 18:16:02 +0300

    New Files Update.

---
+++ 08 - Predefined Modules & Functions/6. PEPs.py
@@ -0,0 +1,5 @@
+# PEP8  : Learn to write code better - Style Guide for Python Code:
+# https://peps.python.org/pep-0008/
+
+# PEP20 : Learn how to think when writing code - The Zen of Python:
+# https://peps.python.org/pep-0020/
