9b34f12d40a7d7498a4cf86a79c47f8b7174963a
TheStxfan <stefan.ferro5@gmail.com>
2024-09-05 17:27:34 +0300

    New Files Update.

---
+++ 08 - Predefined Modules & Functions/3. assign condition result to variable.py
@@ -0,0 +1,19 @@
+# 1
+number = 4
+if number % 2 == 0:
+    isEven = True
+else:
+    isEven = False
+
+print(f"isEven = {isEven}")
+
+# 2
+number = 5
+isEven = True if number % 2 == 0 else False
+
+print(f"isEven = {isEven}")
+
+# 3
+isOdd = 3 % 2 != 0
+print(f"isOdd  = {isOdd}")
+
