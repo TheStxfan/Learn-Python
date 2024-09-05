9500144fd77d35900b8e8046b7698aa2a20a8f13
TheStxfan <stefan.ferro5@gmail.com>
2024-09-05 13:06:48 +0300

    New Files Update.

--- 01 - Input & Output/3. print & input.py
+++ 01 - Input & Output/3. print & input.py
@@ -1,23 +1,27 @@
-print("Hello World!\n")  # \n --> special character - new line
+print("1. Hello World!\n")  # \n --> special character - new line
 
-print("Hello " + "World!\n")  # + --> concatenation without space
+print("2. Hello " + "World!\n")  # + --> concatenation without space
 
-print("Hello", "World!\n")  # , --> concatenation with space
+print("3. Hello", "World!\n")  # , --> concatenation with space
 
-print("Hello")
+print("4. Hello")
 print("World!")  # prints on a new line
 
 print("")  # --> print an empty string (leaves an empty line)
 print()  # --> print an empty line (leaves an empty line)
 
-print("Hello ", end="")  # end="" --> next print or input is on the same line
+print("5. Hello ", end="")  # end="" --> next print or input is on the same line
 print("World!", end="\n")  # end="\n" --> next print or input is on the following line
-print("test\n")
 
-print("Hello", end=" World!")  # end displays " World!" at the end of the line; next print or input is on the same line
-print(" test\n")
+print("6. test\n")
 
-print("Hello",
+print("8. Hello", end=" World!")  # "end" specifies what is displayed at the end of the line
+print(" test\n")                  # the next print or input will be on the same line.
+
+print("10. Hello\"World")  # "\" is an escape character. It tells the code to not treat the next character as command
+print('11. Hello"World')
+
+print("12. Hello",
       end=" World!\n")  # this is another way of end=""
 
 input()  # waits for any user input followed by the enter key before continuing
--- 01 - Input & Output/4. variables.py
+++ 01 - Input & Output/4. variables.py
@@ -1,9 +1,20 @@
-apples = 10  # assigning a value to a variable
+# Assigning values to variables on different lines
+apples = 10
 oranges = 20
-
 print(apples, oranges)  # printing the variables
 
-input("")  # waiting for user input
+
+# Assigning values to variables on the same line
+apples, oranges = 15, 25
+print(apples, oranges)
+
+
+# Assigning the same value to variables on the same line
+number_of_apples = number_of_oranges = 5
+print(number_of_apples, number_of_oranges)
+
+
+input("Press \"Enter\" to continue.")  # waiting for user input
 
 
 # user input can be assigned to a variable
