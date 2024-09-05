151f00e2f20eb06c685e5a73bfa82c0f6a272a64
TheStxfan <stefan.ferro5@gmail.com>
2024-09-05 12:34:02 +0300

    New Files Update.

--- 07 - Advanced Loops/4. sum of divisors.py
+++ 07 - Advanced Loops/4. sum of divisors.py
@@ -1,12 +1,35 @@
+# 1
 def sum_of_divisors(number):
     divisors = []
-    for i in range(1, number+1):
-        if number % i == 0:
-            divisors.append(i)
+    for divisor in range(1, number+1):
+        if number % divisor == 0:
+            divisors.append(divisor)
 
     result = 0
+    total_divisors = len(divisors)
     for divisor in divisors:
+        if divisor == divisors[-1]:
+            print(divisor, end=" = ")
+        else:
+            print(divisor, end=" + ")
         result += divisor
+
+    return result
+
+
+print(sum_of_divisors(15))
+
+
+# 2
+def sum_of_divisors(number):
+    result = 0
+    for divisor in range(1, number+1):
+        if number % divisor == 0:
+            result += divisor
+            if divisor == number:
+                print(divisor, end=" = ")
+            else:
+                print(divisor, end=" + ")
 
     return result
 
