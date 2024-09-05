c883cda7bdbda011c7ffa906827edf161915a7ce
TheStxfan <stefan.ferro5@gmail.com>
2024-09-05 18:59:21 +0300

    New Files Update.

--- 08 - Predefined Modules & Functions/6. PEPs.py
+++ 08 - Predefined Modules & Functions/6. PEPs.py
@@ -1,9 +1,32 @@
-# PEP8  : Learn to write code better - Style Guide for Python Code:
-# https://peps.python.org/pep-0008/
+# PEP 8: Style Guide for Python Code
+# Learn to write code better with PEP 8.
+# More details: https://peps.python.org/pep-0008/
 
-# PEP20 : Learn how to think when writing code - The Zen of Python:
-# https://peps.python.org/pep-0020/
-# This last one is callable directly from a Python console with the following command:
+# PEP 20: The Zen of Python
+# Learn the guiding principles of Python with PEP 20.
+# This can be accessed directly from a Python console with:
 import this
 
-# Run this code ;)
+# There are additional tools to help with PEP guidelines:
+
+# flake8: Checks Python code for PEP 8 compliance.
+# pylint: Analyzes code for errors and coding standards, providing a total score.
+# black: Automatically formats code according to PEP guidelines.
+
+# To install these tools, use:
+# pip install flake8 pylint black
+
+# Usage:
+# Open a terminal (not the Python console) and run:
+
+# 'flake8 path_to_folder' - to analyze multiple scripts (including subfolders)
+# 'flake8 path_to_script' - to analyze a single file
+
+# 'pylint path_to_folder' - to analyze multiple scripts (including subfolders)
+# 'pylint path_to_script' - to analyze a single file
+
+# 'black path_to_folder' - to format multiple scripts (including subfolders)
+# 'black path_to_script' - to format a single file
+
+# Note: This GitHub repo contains mainly .py files for documentation purposes, so the analysis scores may be lower
+# because they are not meant to be complete, functional Python scripts.
