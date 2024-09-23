e0d37d762b26f2dc05ad3789068a92e97b39bb78
TheStxfan <stefan.ferro5@gmail.com>
2024-09-23 21:26:33 +0300

    New Files Update.

---
+++ 14 - Custom Modules/1. custom_module.py
@@ -0,0 +1,13 @@
+# A Python module is simply a Python script containing functions meant to be called from other files.
+
+def method_1():
+    print('method 1')
+
+
+class ClassA:
+    def class_method_1(self):
+        print('method 1')
+
+
+method_1()
+ClassA().class_method_1()
