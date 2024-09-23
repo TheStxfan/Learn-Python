394bb2f4b1212e6f39b8af422f64a3bec3ebe4e4
TheStxfan <stefan.ferro5@gmail.com>
2024-09-23 20:55:13 +0300

    New Files Update.

--- 04 - Functions/6. named parameter.py
+++ 04 - Functions/6. named parameter.py
@@ -1,14 +1,20 @@
 def print_string(string="Hello, World!", n=2):
+    # Print the given string n times.
     for i in range(1, n + 1):
         print(string)
 
 
+# Default usage
 print_string()
 print("\n")
+
+# Custom string
 print_string("Not default")
 print("\n")
+
+# Custom string with specified repetitions
 print_string("Not default", 4)
 print("\n")
-print_string(n=4)
 
-# in "print_string(n=4)", "n=4" is called "named parameter"
+# Using a named parameter for clarity
+print_string(n=4)  # Here, "n=4" is a named parameter.
