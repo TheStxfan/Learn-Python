c9ee94e6125670a68cccf7f57cd51891a67ad407
TheStxfan <stefan.ferro5@gmail.com>
2024-09-23 22:59:47 +0300

    New Files Update.

--- 14 - Custom Modules/0. Section Info - Side Note.py
+++ 14 - Custom Modules/0. Section Info - Side Note.py
@@ -1,2 +1,2 @@
-""" The naming style of the files in this section is different from one in the other sections as
+""" The naming style of some files in this section is different from one in the other sections as
 Module names cannot start with a number or contain spaces. """
